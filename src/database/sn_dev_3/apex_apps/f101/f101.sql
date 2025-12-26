66a8e346c27242aa46d30a1806acd19b06f0219f
sn0313 <ongshinee@gmail.com>
2025-12-26 12:10:45 +0800

    export all the files

--- src/database/sn_dev_3/apex_apps/f101/f101.sql
+++ src/database/sn_dev_3/apex_apps/f101/f101.sql
@@ -36,11 +36,11 @@ prompt APPLICATION 101 - apex-sample-app
 --   Exported By:     SN_DEV_3
 --   Flashback:       0
 --   Export Type:     Application Export
---     Pages:                     26
+--     Pages:                     28
 --       Items:                   41
 --       Validations:              2
 --       Processes:               21
---       Regions:                 63
+--       Regions:                 68
 --       Buttons:                 37
 --       Dynamic Actions:         16
 --     Shared Components:
@@ -50,7 +50,7 @@ prompt APPLICATION 101 - apex-sample-app
 --       Navigation:
 --         Lists:                  8
 --         Breadcrumbs:            1
---           Entries:              2
+--           Entries:              4
 --       Security:
 --         Authentication:         1
 --         Authorization:          3
@@ -112,7 +112,7 @@ wwv_imp_workspace.create_flow(
 ,p_substitution_value_01=>'apex-sample-app'
 ,p_file_prefix => nvl(wwv_flow_application_install.get_static_app_file_prefix,'')
 ,p_files_version=>6
-,p_version_scn=>45908677468678
+,p_version_scn=>45908708215705
 ,p_print_server_type=>'NATIVE'
 ,p_file_storage=>'DB'
 ,p_is_pwa=>'Y'
@@ -163,7 +163,7 @@ wwv_flow_imp_shared.create_list(
  p_id=>wwv_flow_imp.id(20920933440764354)
 ,p_name=>'Navigation Menu'
 ,p_list_status=>'PUBLIC'
-,p_version_scn=>45905885698387
+,p_version_scn=>45908707671823
 );
 wwv_flow_imp_shared.create_list_item(
  p_id=>wwv_flow_imp.id(20935949821764541)
@@ -172,6 +172,24 @@ wwv_flow_imp_shared.create_list_item(
 ,p_list_item_link_target=>'f?p=&APP_ID.:1:&APP_SESSION.::&DEBUG.:::'
 ,p_list_item_icon=>'fa-home'
 ,p_list_item_current_type=>'TARGET_PAGE'
+);
+wwv_flow_imp_shared.create_list_item(
+ p_id=>wwv_flow_imp.id(11233663731457765)
+,p_list_item_display_sequence=>20
+,p_list_item_link_text=>'Dev Page'
+,p_list_item_link_target=>'f?p=&APP_ID.:3:&APP_SESSION.::&DEBUG.:::'
+,p_list_item_icon=>'fa-file-o'
+,p_list_item_current_type=>'COLON_DELIMITED_PAGE_LIST'
+,p_list_item_current_for_pages=>'3'
+);
+wwv_flow_imp_shared.create_list_item(
+ p_id=>wwv_flow_imp.id(11235613765463310)
+,p_list_item_display_sequence=>30
+,p_list_item_link_text=>'RC Fix Page'
+,p_list_item_link_target=>'f?p=&APP_ID.:2:&APP_SESSION.::&DEBUG.:::'
+,p_list_item_icon=>'fa-file-o'
+,p_list_item_current_type=>'COLON_DELIMITED_PAGE_LIST'
+,p_list_item_current_for_pages=>'2'
 );
 wwv_flow_imp_shared.create_list_item(
  p_id=>wwv_flow_imp.id(21112097352774311)
@@ -1249,6 +1267,18 @@ wwv_flow_imp_shared.create_menu(
 ,p_name=>'Breadcrumb'
 );
 wwv_flow_imp_shared.create_menu_option(
+ p_id=>wwv_flow_imp.id(11234541810457770)
+,p_short_name=>'Dev Page'
+,p_link=>'f?p=&APP_ID.:3:&APP_SESSION.::&DEBUG.:::'
+,p_page_id=>3
+);
+wwv_flow_imp_shared.create_menu_option(
+ p_id=>wwv_flow_imp.id(11236545557463314)
+,p_short_name=>'RC Fix Page'
+,p_link=>'f?p=&APP_ID.:2:&APP_SESSION.::&DEBUG.:::'
+,p_page_id=>2
+);
+wwv_flow_imp_shared.create_menu_option(
  p_id=>wwv_flow_imp.id(20920676401764349)
 ,p_short_name=>'Home'
 ,p_link=>'f?p=&APP_ID.:1:&APP_SESSION.::&DEBUG.:::'
@@ -1490,6 +1520,94 @@ wwv_flow_imp_page.create_page_plug(
   'expand_shortcuts', 'N',
   'output_as', 'HTML',
   'show_line_breaks', 'Y')).to_clob
+);
+end;
+/
+prompt --application/pages/page_00002
+begin
+wwv_flow_imp_page.create_page(
+ p_id=>2
+,p_name=>'RC Fix Page'
+,p_alias=>'RC-FIX-PAGE'
+,p_step_title=>'RC Fix Page'
+,p_autocomplete_on_off=>'OFF'
+,p_page_template_options=>'#DEFAULT#'
+,p_protection_level=>'C'
+,p_page_component_map=>'11'
+);
+wwv_flow_imp_page.create_page_plug(
+ p_id=>wwv_flow_imp.id(10080349774683113)
+,p_plug_name=>'New'
+,p_region_template_options=>'#DEFAULT#:t-Region--scrollBody'
+,p_plug_template=>4072358936313175081
+,p_plug_display_sequence=>10
+,p_location=>null
+,p_attributes=>wwv_flow_t_plugin_attributes(wwv_flow_t_varchar2(
+  'expand_shortcuts', 'N',
+  'output_as', 'HTML')).to_clob
+);
+wwv_flow_imp_page.create_page_plug(
+ p_id=>wwv_flow_imp.id(10080451187683114)
+,p_plug_name=>'New'
+,p_parent_plug_id=>wwv_flow_imp.id(10080349774683113)
+,p_region_template_options=>'#DEFAULT#:t-Region--scrollBody'
+,p_plug_template=>4072358936313175081
+,p_plug_display_sequence=>10
+,p_location=>null
+,p_plug_source=>'<h1>RC TEST PAGE.</h1>'
+,p_attributes=>wwv_flow_t_plugin_attributes(wwv_flow_t_varchar2(
+  'expand_shortcuts', 'N',
+  'output_as', 'HTML')).to_clob
+);
+wwv_flow_imp_page.create_page_plug(
+ p_id=>wwv_flow_imp.id(11236057307463311)
+,p_plug_name=>'Breadcrumb'
+,p_region_template_options=>'#DEFAULT#:t-BreadcrumbRegion--useBreadcrumbTitle'
+,p_component_template_options=>'#DEFAULT#'
+,p_plug_template=>2531463326621247859
+,p_plug_display_sequence=>10
+,p_plug_display_point=>'REGION_POSITION_01'
+,p_menu_id=>wwv_flow_imp.id(20920439052764348)
+,p_plug_source_type=>'NATIVE_BREADCRUMB'
+,p_menu_template_id=>4072363345357175094
+);
+end;
+/
+prompt --application/pages/page_00003
+begin
+wwv_flow_imp_page.create_page(
+ p_id=>3
+,p_name=>'Dev Page'
+,p_alias=>'DEV-PAGE'
+,p_step_title=>'Dev Page'
+,p_autocomplete_on_off=>'OFF'
+,p_page_template_options=>'#DEFAULT#'
+,p_protection_level=>'C'
+,p_page_component_map=>'11'
+);
+wwv_flow_imp_page.create_page_plug(
+ p_id=>wwv_flow_imp.id(10080282362683112)
+,p_plug_name=>'New'
+,p_region_template_options=>'#DEFAULT#:t-Region--scrollBody'
+,p_plug_template=>4072358936313175081
+,p_plug_display_sequence=>10
+,p_location=>null
+,p_plug_source=>'<h1>DEV PAGE.</h1>'
+,p_attributes=>wwv_flow_t_plugin_attributes(wwv_flow_t_varchar2(
+  'expand_shortcuts', 'N',
+  'output_as', 'HTML')).to_clob
+);
+wwv_flow_imp_page.create_page_plug(
+ p_id=>wwv_flow_imp.id(11234089818457767)
+,p_plug_name=>'Breadcrumb'
+,p_region_template_options=>'#DEFAULT#:t-BreadcrumbRegion--useBreadcrumbTitle'
+,p_component_template_options=>'#DEFAULT#'
+,p_plug_template=>2531463326621247859
+,p_plug_display_sequence=>10
+,p_plug_display_point=>'REGION_POSITION_01'
+,p_menu_id=>wwv_flow_imp.id(20920439052764348)
+,p_plug_source_type=>'NATIVE_BREADCRUMB'
+,p_menu_template_id=>4072363345357175094
 );
 end;
 /
@@ -7269,4 +7387,4 @@ prompt  ...done
 
 
 
--- sqlcl_snapshot {"hash":"37bc3069a0c55f61c74b439d1f3b464665431314","type":"APEX_APPLICATIONS","name":"f101","schemaName":"SN_DEV_3","sxml":""}
+-- sqlcl_snapshot {"hash":"4331a4efac843a51307984be8704affae343a1d5","type":"APEX_APPLICATIONS","name":"f101","schemaName":"SN_DEV_3","sxml":""}
